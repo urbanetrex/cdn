c8c666ce1d4defb6c9c75c7f7b96359afa9904b8
urbanetrex <urbanetrex@gmail.com>
2025-06-17 18:04:35 +1000

    Publish, v1.3

---
+++ html/SM-LVD/releases/releases.js
@@ -0,0 +1,41 @@
+export const releases = {
+    "1.0": {
+        "time-tag": "2025-06-03T07:19:00",
+        "time-text": "June 3, 2025 at 7:19 AM",
+        "features": [
+            "Initial release with basic features.",
+            "Had a gradient banner and cloudy background."
+        ]
+    },
+
+    "1.1": {
+        "time-tag": "2025-06-07T06:08:00",
+        "time-text": "June 7, 2025 at 6:08 AM",
+        "features": [
+            "Updated plural section, with an exclusives page.",
+            "You can now press enter to submit, not just using the button.",
+            "Now if you disable JS it will show a message indicating that JavaScript is required.",
+            "Added a home button on every page other than the main page."
+        ]
+    },
+
+    "1.2": {
+        "time-tag": "2025-06-16T08:00:00",
+        "time-text": "June 16, 2025 at 8:00 AM",
+        "features": [
+            "Added the releases page.",
+            "With the releases page, I added some releases button, and styled the buttons some more."
+        ]
+    },
+
+    "1.3": {
+        "time-tag": "2025-06-17T18:05:00",
+        "time-text": "June 17, 2025 at 6:05 PM",
+        "features": [
+            "JavaScript is now required to view the releases page.",
+            "Added a pagination system to the releases page.",
+            "Added a hint message when you reach the oldest release or newest release.",
+            "Buttons are animated when hovered over."
+        ]
+    }
+}
